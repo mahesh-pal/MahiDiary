6dfd64b9fd053c55621eab3ad72823c2e20c32c8
Mahesh-pal <rathaur.m7@gmail.com>
2020-06-07 18:56:10 +0530

    infinite scroll

---
+++ src/app/service/pagination_service.ts
@@ -0,0 +1,116 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { BehaviorSubject, Observable } from 'rxjs';
+import {tap, scan, take} from 'rxjs/operators';
+import {Blog} from '../models/blog';
+
+// Options to reproduce firestore queries consistently
+interface QueryConfig {
+  path: string; // path to collection
+  field: string; // field to orderBy
+  limit?: number; // limit per query
+  reverse?: boolean; // reverse order?
+  prepend?: boolean; // prepend to source?
+}
+
+@Injectable({providedIn: 'root'})
+export class PaginationService {
+
+  // Source data
+  private _done = new BehaviorSubject(false);
+  private _loading = new BehaviorSubject(false);
+  private _data = new BehaviorSubject([]);
+
+  private query: QueryConfig;
+
+  // Observable data
+  data: Observable<any>;
+  done: Observable<boolean> = this._done.asObservable();
+  loading: Observable<boolean> = this._loading.asObservable();
+
+  constructor( private afs: AngularFirestore ) { }
+
+  // Initial query sets options and defines the Observable
+  init(path, field, opts?) {
+    this.query = {
+      path,
+      field,
+      limit: 10,
+      reverse: false,
+      prepend: false,
+      ...opts
+    };
+
+    const first = this.afs.collection<Blog>(this.query.path, ref => {
+      return ref
+              .orderBy(this.query.field, 'asc')
+              .limit(this.query.limit);
+              // .where('categoria','==', 'Herramientas y Construcción' )
+    });
+
+    this.mapAndUpdate(first);
+
+    // Create the observable array for consumption in components
+    this.data = this._data.asObservable().pipe(scan( (acc, val) => {
+        return  acc.concat(val);
+      }));
+  }
+
+  // Retrieves additional data from firestore
+  more() {
+    const cursor = this.getCursor();
+
+    const more = this.afs.collection<Blog>(this.query.path, ref => {
+      return ref
+              .orderBy(this.query.field, this.query.reverse ? 'desc' : 'asc')
+              .limit(this.query.limit)
+              .startAfter(cursor);
+    });
+    this.mapAndUpdate(more);
+  }
+
+  // Determines the doc snapshot to paginate query
+  private getCursor() {
+    const current = this._data.value;
+    if (current.length) {
+      return  current[current.length - 1].doc;
+    }
+    return null;
+  }
+
+
+  // Maps the snapshot to usable format the updates source
+  private mapAndUpdate(col: AngularFirestoreCollection<Blog>) {
+
+    if (this._done.value || this._loading.value) {
+         return ;
+    }
+
+    // loading
+    this._loading.next(true);
+
+    // Map snapshot with doc ref (needed for cursor)
+    return col.snapshotChanges().pipe(tap(arr => {
+        let values = arr.map(snap => {
+          const data = snap.payload.doc.data();
+          const doc = snap.payload.doc;
+          return { ...data, doc };
+        });
+
+        // update source with new values, done loading
+        this._data.next(values);
+        this._loading.next(false);
+
+        // no more values, mark done
+        if (!values.length) {
+          this._done.next(true);
+        }
+    }), take(1)).subscribe();
+  }
+
+  // Reset the page
+  reset() {
+    this._data.next([]);
+    this._done.next(false);
+  }
+}
